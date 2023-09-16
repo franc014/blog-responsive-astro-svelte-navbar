921adccedc6151e825d778ee7018531b7129ccd4
JUan Andrade <jfandradea@gmail.com>
2023-09-15 20:07:46 -0500

    initial commit

---
+++ src/components/svelte/Menu.svelte
@@ -0,0 +1,25 @@
+<script>
+  import MenuBtn from "./MenuBtn.svelte";
+  import CloseBtn from "./CloseBtn.svelte";
+
+  const navIsOpen = true;
+</script>
+
+<div class="self-end pt-2 pr-4 bg-violet-200">
+  <MenuBtn />
+  <CloseBtn />
+</div>
+<nav class:navIsOpen class="bg-pink-50 w-full opacity-0">
+  <ul class="text-center">
+    <li><a href="/">About</a></li>
+    <li><a href="/">Products</a></li>
+    <li><a href="/">Reviews</a></li>
+    <li><a href="/">Contact</a></li>
+  </ul>
+</nav>
+
+<style>
+  .navIsOpen {
+    opacity: 1;
+  }
+</style>
