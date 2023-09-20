dbe01974e5d08e48e74d372e62a8fc215ac087ee
JUan Andrade <jfandradea@gmail.com>
2023-09-19 20:02:52 -0500

    refactor btn components

--- src/components/svelte/CloseBtn.svelte
+++ src/components/svelte/CloseBtn.svelte
@@ -1,19 +1,17 @@
 <script>
-  import {navIsOpen} from './stores';
-
-  function toggleNavbar(e) {
-    navIsOpen.update((open)=>!open);
-  }
+  import { navIsOpen } from "./stores";
+  import NavBtn from "./NavBtn.svelte";
 </script>
 
-<button class:navIsOpen={$navIsOpen} class="hidden" on:click={toggleNavbar}
-  ><svg
+<NavBtn>
+  <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
     stroke-width="1.5"
     stroke="currentColor"
-    class="w-10 h-10 text-slate-200"
+    class="w-10 h-10 text-slate-200 hidden"
+    class:navIsOpen={$navIsOpen}
   >
     <path
       stroke-linecap="round"
@@ -21,7 +19,7 @@
       d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
     />
   </svg>
-</button>
+</NavBtn>
 
 <style>
   .navIsOpen {
---
+++ src/components/svelte/NavBtn.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { navIsOpen } from "./stores";
+
+  function toggleNavbar() {
+    navIsOpen.update((open) => !open);
+  }
+</script>
+
+<button on:click={toggleNavbar}>
+  <slot />
+</button>
