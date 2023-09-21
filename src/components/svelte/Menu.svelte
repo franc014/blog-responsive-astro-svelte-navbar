faf813e01f7179f1eb4a6ca8e69f4511655db7de
JUan Andrade <jfandradea@gmail.com>
2023-09-20 20:50:42 -0500

    add transitions

--- src/components/svelte/Menu.svelte
+++ src/components/svelte/Menu.svelte
@@ -1,27 +1,30 @@
 <script>
-  import {navIsOpen} from './stores';
+  import { navIsOpen } from "./stores";
   import MenuBtn from "./MenuBtn.svelte";
   import CloseBtn from "./CloseBtn.svelte";
-
-  /* let navIsOpen = false;
-
-  function toggleNavbar() {
-    navIsOpen = !navIsOpen;
-  } */
+  import { fly, fade } from "svelte/transition";
+  import { sineIn } from "svelte/easing";
 </script>
 
 <div class="ml-auto p-4">
   <MenuBtn />
   <CloseBtn />
 </div>
-<nav class:navIsOpen={$navIsOpen} class="w-full hidden py-8">
-  <ul class="text-center text-2xl tracking-wider text-slate-200 ">
-    <li><a href="/">About</a></li>
-    <li><a href="/">Products</a></li>
-    <li><a href="/">Reviews</a></li>
-    <li><a href="/">Contact</a></li>
-  </ul>
-</nav>
+{#key $navIsOpen}
+  <nav
+    in:fly={{ y: -200, duration: 1000, easing: sineIn }}
+    out:fly={{ x: -200 }}
+    class:navIsOpen={$navIsOpen}
+    class="w-full hidden py-8"
+  >
+    <ul class="text-center text-2xl tracking-wider text-slate-200">
+      <li><a href="/">About</a></li>
+      <li><a href="/">Products</a></li>
+      <li><a href="/">Reviews</a></li>
+      <li><a href="/">Contact</a></li>
+    </ul>
+  </nav>
+{/key}
 
 <style>
   .navIsOpen {
--- src/components/svelte/MenuBtn.svelte
+++ src/components/svelte/MenuBtn.svelte
@@ -1,25 +1,29 @@
 <script>
   import { navIsOpen } from "./stores";
   import NavBtn from "./NavBtn.svelte";
+  import { fly, fade, scale } from "svelte/transition";
 </script>
 
-<NavBtn>
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    stroke-width="1.5"
-    stroke="currentColor"
-    class="w-10 h-10 text-slate-200 block"
-    class:navIsOpen={$navIsOpen}
-  >
-    <path
-      stroke-linecap="round"
-      stroke-linejoin="round"
-      d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
-    />
-  </svg>
-</NavBtn>
+{#key $navIsOpen}
+  <NavBtn>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-10 h-10 text-slate-200 block"
+      class:navIsOpen={$navIsOpen}
+      transition:scale
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M3.75 5.25h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5m-16.5 4.5h16.5"
+      />
+    </svg>
+  </NavBtn>
+{/key}
 
 <style>
   .navIsOpen {
